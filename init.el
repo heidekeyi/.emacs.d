2e58492bd273d7057678b0e3823d7920e4390d23
heidekeyi <1213173752@qq.com>
2021-08-18 22:46:14 +0800

    fix copy bug

--- init.el
+++ init.el
@@ -18,21 +18,21 @@
 
 
 
-;; 设置键盘输入时的字符编码
-(set-keyboard-coding-system 'utf-8)
-(set-selection-coding-system 'utf-8)
+;; ;; 设置键盘输入时的字符编码
+;; (set-keyboard-coding-system 'utf-8)
+;; (set-selection-coding-system 'utf-8)
 ;; 文件默认保存为 utf-8
 (set-buffer-file-coding-system 'utf-8)
 (set-default buffer-file-coding-system 'utf8)
 (set-default-coding-systems 'utf-8)
-;; 解决粘贴中文出现乱码的问题
-(set-clipboard-coding-system 'utf-8)
-;; 终端中文乱码
-(set-terminal-coding-system 'utf-8)
-(modify-coding-system-alist 'process "*" 'utf-8)
-(setq default-process-coding-system '(utf-8 . utf-8))
-(setq-default pathname-coding-system 'utf-8)
-(set-file-name-coding-system 'utf-8)
+;; ;; 解决粘贴中文出现乱码的问题
+;; (set-clipboard-coding-system 'utf-8)
+;; ;; 终端中文乱码
+;; (set-terminal-coding-system 'utf-8)
+;; (modify-coding-system-alist 'process "*" 'utf-8)
+;; (setq default-process-coding-system '(utf-8 . utf-8))
+;; (setq-default pathname-coding-system 'utf-8)
+;; (set-file-name-coding-system 'utf-8)
 ;; 不设置 interative-haskell-mode中进入ghci中会卡死（不停提示进程死亡）
 (set-language-environment 'utf-8)
 
@@ -218,5 +218,4 @@
 
 ;; 补全引号、括号之类的
 (smartparens-global-mode t)
-
 
